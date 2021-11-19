821cf1ae1e4a0e13d7637ca4f1829afc49d51dfc
gavinyan123 <89555841+gavinyan123@users.noreply.github.com>
2021-11-19 03:21:32 -0500

    Completed Credit and Debit
    addCredit/addDebit:
         successfully adds description, time and amount to the list 
         successfully adds/subtract amount to/from account balance
    
    including JSON files

--- components/Credits.js
+++ components/Credits.js
@@ -11,16 +11,22 @@ const Credits = (props) => {
         }) 
     }
     return (
-    	<div>
-    	   <h1>Credits</h1>
-    	   {creditsView()}
-           <form onSubmit={props.addCredit}>
-             <input type="text" name="description" />
-             <input type="number" name="amount" />
-             <button type="submit">Add Credit</button>
-           </form>
-		   <Link to="/">Return to Home</Link>
-    	</div>
+      <div>
+        <h1>Credits</h1>
+        <p1>
+          <AccountBalance accountBalance = {props.accountBalance}/>
+        </p1>
+        {creditsView()}
+          <form onSubmit={props.addCredit}>
+            <input type="text" name="description" />
+            <input type="number" name="amount" />
+            <button type="submit">Add Credit</button>
+          </form>
+        <Link to="/userProfile"><button type = "button"> User Profile </button></Link>
+        <Link to="LogIn"><button type = "button"> Log In </button></Link>
+        <Link to="/Debits"><button type = "button">Debits</button></Link>
+        <Link to="/"><button type = "button">Return to Home</button></Link>
+    </div>
     )
 }
 export default Credits;
--- components/Debits.js
+++ components/Debits.js
@@ -1,26 +1,32 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
 import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
 
 const Debits = (props) => {
 	let debitsView = () => {
-        const { debits } = props;
-        return debits.map((debit) => {
-            let date = debit.date.slice(0,10);
-            return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
-        }) 
+    const { debits } = props;
+    return debits.map((debit) => {
+      let date = debit.date.slice(0,10);
+      return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+    }) 
     }
     return (
-    	<div>
-    	   <h1>Debits</h1>
-    	   {debitsView()}
-           <form onSubmit={props.addDebit}>
-             <input type="text" name="description" />
-             <input type="number" name="amount" />
-             <button type="submit">Add Debit</button>
-           </form>
-		   <Link to="/">Return to Home</Link>
-    	</div>
+      <div>
+        <h1>Debits</h1>
+        <p1>
+          <AccountBalance accountBalance = {props.accountBalance}/>
+        </p1>
+        {debitsView()}
+          <form onSubmit={props.addDebit}>
+            <input type="text" name="description" />
+            <input type="number" name="amount" />
+            <button type="submit">Add Debit</button>
+          </form>
+        <Link to="/userProfile"><button type = "button"> User Profile </button></Link>
+        <Link to="LogIn"><button type = "button"> Log In </button></Link>
+        <Link to="/Credits"><button type = "button">Credits</button></Link>
+        <Link to="/"><button type = "button">Return to Home</button></Link>
+      </div>
     )
 }
 export default Debits;
