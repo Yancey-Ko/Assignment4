def814300c6beaa7210b866d7abe22c90615f758
Yancey-Ko <57046017+Yancey-Ko@users.noreply.github.com>
2021-11-21 18:08:20 -0500

    Update Debits.js

--- components/Debits.js
+++ components/Debits.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
-
+//Displays debitit data from API and creates form to add more to debit.
 const Debits = (props) => {
 	let debitsView = () => {
     const { debits } = props;
@@ -13,15 +13,14 @@ const Debits = (props) => {
     return (
       <div>
         <h1>Debits</h1>
-        <p1>
-          <AccountBalance accountBalance = {props.accountBalance}/>
-        </p1>
+          
         {debitsView()}
           <form onSubmit={props.addDebit}>
             <input type="text" name="description" />
             <input type="number" name="amount" />
             <button type="submit">Add Debit</button>
           </form>
+		  <AccountBalance accountBalance = {props.accountBalance}/>
         <Link to="/userProfile"><button type = "button"> User Profile </button></Link>
         <Link to="LogIn"><button type = "button"> Log In </button></Link>
         <Link to="/Credits"><button type = "button">Credits</button></Link>
